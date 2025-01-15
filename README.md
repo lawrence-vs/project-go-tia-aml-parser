c5aa69ef417d31b8f44be2a395209b93202e87d4
Lawrence van Staden <lawrence.vanstaden@siemens.com>
2025-01-15 13:38:37 +0200

    MVP working, can export aml data to excel, adding format next

--- README.md
+++ README.md
@@ -8,11 +8,25 @@ The TIA Selection Tool (TST) can be used to configure siemens automation harware
 This project aim to create an automation the IO list creation using an exported AutomationML file.
 
 ## Getting Started
-
+1. install go dependencies
+    ```bash
+    cd app/src
+    go mod init tia-aml-parser
+    go install github.com/xuri/excelize/v2@latest
+    go mod tidy
+    go build -o tia-aml-parser
+    cd ../../
+    ```
 1. copy your aml file to the app directory.
 2. then run the sciprt to export the aml file data to an excel IO list.
-
+- manually
 ```bash
 cd app/src
 go run main.go
+```
+
+- using powershell
+
+```
+./run.ps1
 ```
--- app/src/main.go
+++ app/src/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/xml"
 	"fmt"
 	"os"
+
+	"github.com/xuri/excelize/v2"
 )
 
 // Structs to represent the XML hierarchy
@@ -45,9 +47,57 @@ func processInternalElements(elements []InternalElement, depth int) {
 	}
 }
 
+// Export the parsed data to an Excel file
+func exportToExcel(instanceHierarchy InstanceHierarchy, filePath string) error {
+	f := excelize.NewFile()
+	sheetName := "InstanceHierarchy"
+
+	// Create a new sheet
+	index, err := f.NewSheet(sheetName)
+	if err != nil {
+		return fmt.Errorf("failed to create sheet: %v", err)
+	}
+	f.SetActiveSheet(index)
+
+	// Add headers
+	headers := []string{"Element ID", "Element Name", "Attribute Name", "Attribute Type", "Attribute Value"}
+	for col, header := range headers {
+		cell := fmt.Sprintf("%s1", string('A'+col))
+		f.SetCellValue(sheetName, cell, header)
+	}
+
+	// Helper function to populate rows
+	var rowIndex int = 2
+	var writeRows func(elements []InternalElement, depth int)
+
+	writeRows = func(elements []InternalElement, depth int) {
+		for _, elem := range elements {
+			for _, attr := range elem.Attribute {
+				f.SetCellValue(sheetName, fmt.Sprintf("A%d", rowIndex), elem.ID)
+				f.SetCellValue(sheetName, fmt.Sprintf("B%d", rowIndex), elem.Name)
+				f.SetCellValue(sheetName, fmt.Sprintf("C%d", rowIndex), attr.Name)
+				f.SetCellValue(sheetName, fmt.Sprintf("D%d", rowIndex), attr.Type)
+				f.SetCellValue(sheetName, fmt.Sprintf("E%d", rowIndex), attr.Value)
+				rowIndex++
+			}
+			writeRows(elem.InternalElement, depth+1)
+		}
+	}
+
+	// Write data to the Excel file
+	writeRows(instanceHierarchy.InternalElement, 0)
+
+	// Save the Excel file
+	if err := f.SaveAs(filePath); err != nil {
+		return fmt.Errorf("failed to save Excel file: %v", err)
+	}
+
+	return nil
+}
+
 func main() {
 	// Open the XML file
-	xmlFile, err := os.Open("./app/example.xml")
+	xmlFile, err := os.Open("../example.xml")
 	if err != nil {
 		fmt.Println("Error opening XML file:", err)
 		return
@@ -67,4 +117,14 @@ func main() {
 	fmt.Println("Automation IO List:")
 	fmt.Println("====================")
 	processInternalElements(caexFile.InstanceHierarchy.InternalElement, 0)
+
+	// Export to Excel
+	excelPath := "../test.xlsx"
+	err = exportToExcel(caexFile.InstanceHierarchy, excelPath)
+	if err != nil {
+		fmt.Printf("Error exporting to Excel: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Data successfully exported to %s\n", excelPath)
 }
